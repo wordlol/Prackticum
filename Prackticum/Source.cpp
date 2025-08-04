054e930b9e28d9ce331c3636794f1a7e6dd0dbf3
Mihail Tumanov <tumanov.mike@yandex.ru>
2025-08-04 21:43:20 +0400

    Добавьте файлы проекта.

---
+++ Prackticum/Source.cpp
@@ -0,0 +1,19 @@
+#include "programms.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	//FirstCode();
+	//Operators();
+	//Logic();
+	//WhatNumber();
+	//WhatWord();
+	//StoreGoods();
+	//TextAdvacuresLocation();
+	//TextAdvacuresDoor();
+	TextAdvacuresItems();
+
+	return 0;
+}
